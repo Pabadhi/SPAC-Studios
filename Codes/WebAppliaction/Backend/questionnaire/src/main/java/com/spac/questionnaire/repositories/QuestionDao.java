138e0b8dd093cdf9e2919d571c0af23b5b39a799
Asini Jayakody <jayakodyasini@gmail.com>
2024-03-31 19:11:06 +0530

    .

--- Codes/WebAppliaction/Backend/questionnaire/src/main/java/com/spac/questionnaire/repositories/QuestionDao.java
+++
@@ -1,9 +0,0 @@
-package com.spac.questionnaire.repositories;
-
-import com.spac.questionnaire.dao.Question;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface QuestionDao extends JpaRepository<Question,Integer> {
-}
