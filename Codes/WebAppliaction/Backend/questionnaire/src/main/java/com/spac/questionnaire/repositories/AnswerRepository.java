4911dc4559b4e2848e8c3ad77ead28a189df9076
Asini Jayakody <jayakodyasini@gmail.com>
2024-03-31 19:13:53 +0530

    .

--- Codes/WebAppliaction/Backend/questionnaire/src/main/java/com/spac/questionnaire/repositories/AnswerRepository.java
+++ Codes/WebAppliaction/Backend/questionnaire/src/main/java/com/spac/questionnaire/repositories/AnswerRepository.java
@@ -2,9 +2,11 @@ package com.spac.questionnaire.repositories;
 
 import com.spac.questionnaire.dao.Answer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface AnswerRepository extends JpaRepository<Answer,Integer> {
     List<Answer> findByQuestionId(Integer id);
 }
