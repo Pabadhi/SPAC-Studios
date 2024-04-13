a7c8e7c8eeb5a41615466827bb7d968a8f8cabfb
Asini Jayakody <jayakodyasini@gmail.com>
2024-04-13 20:02:42 +0530

    .

--- Codes/WebAppliaction/Backend/questionnaire/src/main/java/com/spac/questionnaire/security/SecurityConfiguration.java
+++ Codes/WebAppliaction/Backend/questionnaire/src/main/java/com/spac/questionnaire/security/SecurityConfiguration.java
@@ -10,9 +10,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.Arrays;
 
@@ -25,26 +23,19 @@ public class SecurityConfiguration {
     JwtAuthenticationFilter jwtAuthenticationFilter;
     @Autowired
     AuthenticationProvider authenticationProvider;
-    
 
-    @Bean
-    public CorsConfigurationSource corsConfigurationSource(){
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
-        configuration.setAllowedMethods(Arrays.asList("GET","POST"));
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**",configuration);
-        return source;
-    }
+    @Autowired
+    CorsConfigurationSource corsConfigurationSource;
+
 
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
         http
-                .cors(c-> c.configurationSource(corsConfigurationSource()))
+                .cors(c-> c.configurationSource(corsConfigurationSource))
                 .csrf().disable()
                 .authorizeHttpRequests()
-                .requestMatchers("api/auth/*")
+                .requestMatchers("api/auth/**")
                 .permitAll()
                 .anyRequest()
                 .authenticated()
