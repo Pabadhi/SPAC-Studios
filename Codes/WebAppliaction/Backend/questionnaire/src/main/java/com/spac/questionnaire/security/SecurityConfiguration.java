3433ff5016dcdaf3fab5377e6121ff0b37d01874
Asini Jayakody <jayakodyasini@gmail.com>
2024-04-13 20:35:18 +0530

    .

--- Codes/WebAppliaction/Backend/questionnaire/src/main/java/com/spac/questionnaire/security/SecurityConfiguration.java
+++ Codes/WebAppliaction/Backend/questionnaire/src/main/java/com/spac/questionnaire/security/SecurityConfiguration.java
@@ -12,7 +12,6 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfigurationSource;
 
-import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
