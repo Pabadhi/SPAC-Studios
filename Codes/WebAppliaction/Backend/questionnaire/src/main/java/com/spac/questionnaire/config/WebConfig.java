1a803faca818776b58561e92b8b71a74098357a7
Asini Jayakody <jayakodyasini@gmail.com>
2024-04-13 19:25:45 +0530

    port change

--- Codes/WebAppliaction/Backend/questionnaire/src/main/java/com/spac/questionnaire/config/WebConfig.java
+++ Codes/WebAppliaction/Backend/questionnaire/src/main/java/com/spac/questionnaire/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource(){
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
         configuration.setAllowedMethods(Arrays.asList("GET","POST"));
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**",configuration);
