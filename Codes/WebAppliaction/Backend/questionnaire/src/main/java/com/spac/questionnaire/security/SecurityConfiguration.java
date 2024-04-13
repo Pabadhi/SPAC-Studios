87bd0565d48f01e102d7ee6b07bc22a00178ae39
Asini Jayakody <jayakodyasini@gmail.com>
2024-04-13 19:38:11 +0530

    .

--- Codes/WebAppliaction/Backend/questionnaire/src/main/java/com/spac/questionnaire/security/SecurityConfiguration.java
+++ Codes/WebAppliaction/Backend/questionnaire/src/main/java/com/spac/questionnaire/security/SecurityConfiguration.java
@@ -10,7 +10,11 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
@@ -21,15 +25,23 @@ public class SecurityConfiguration {
     JwtAuthenticationFilter jwtAuthenticationFilter;
     @Autowired
     AuthenticationProvider authenticationProvider;
+    
 
-    @Autowired
-    CorsConfigurationSource corsConfigurationSource;
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource(){
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+        configuration.setAllowedMethods(Arrays.asList("GET","POST"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**",configuration);
+        return source;
+    }
 
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
         http
-                .cors(c-> c.configurationSource(corsConfigurationSource))
+                .cors(c-> c.configurationSource(corsConfigurationSource()))
                 .csrf().disable()
                 .authorizeHttpRequests()
                 .requestMatchers("api/auth/*")
